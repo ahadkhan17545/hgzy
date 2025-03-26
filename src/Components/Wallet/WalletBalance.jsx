e3de94e4267e3c5cde8d8c3a246dc515a873d28f
vismodeb <vismodeb2000@gmail.com>
2025-03-26 23:15:44 +0600

    Wallwt page design

---
+++ src/Components/Wallet/WalletBalance.jsx
@@ -0,0 +1,87 @@
+import { IoIosWallet } from "react-icons/io";
+import { Link } from "react-router-dom";
+
+const WalletBalance = () => {
+  return (
+    <div className="bg-backgroundWhite">
+      <div className="pt-10 pb-16">
+        <div className="pt-6 pb-2 text-white bg-red text-center">
+          <IoIosWallet size={40} className="w-30 m-auto" />
+          <span className="text-3xl font-semibold">৳0.00</span>
+          <p className="text-sm font-normal">Total balance</p>
+        </div>
+
+        {/* menu */}
+        <div className="px-2 sm:px-4 py-4">
+          <div className="px-2 py-4 rounded-md bg-white space-y-4">
+            <div className="flex justify-around gap-1 items-center">
+              <div className="text-center">
+                <span className="text-base font-semibold">৳0.00</span>
+                <p className="text-sm font-normal">Main wallet</p>
+              </div>
+              <div className="text-center">
+                <span className="text-base font-semibold">৳0.00</span>
+                <p className="text-sm font-normal">3rd party wallet</p>
+              </div>
+            </div>
+            <button className="py-2 px-2 rounded-full text-white text-xl font-semibold bg-red w-full">
+              Main wallet transfer
+            </button>
+            <div className="flex justify-around gap-1">
+              <Link>
+                <div className="">
+                  <img
+                    className="w-12 h-12 m-auto"
+                    src="https://hgzy.org/assets/png/rechargeIcon-e515aee4.png"
+                    alt=""
+                  />
+                  <p className="text-sm w-12 text-center text-text">Deposit</p>
+                </div>
+              </Link>
+              <Link>
+                <div className="">
+                  <img
+                    className="w-12 h-12 m-auto"
+                    src="https://hgzy.org/assets/png/widthdrawBlue-80197e64.png"
+                    alt=""
+                  />
+                  <p className="text-sm w-12 text-center text-text">Withdraw</p>
+                </div>
+              </Link>
+              <Link>
+                <div className="">
+                  <img
+                    className="w-12 h-12 m-auto"
+                    src="https://hgzy.org/assets/png/rechargeHistory-b5a853c0.png"
+                    alt=""
+                  />
+                  <p className="text-sm w-12 text-center text-text">
+                    Deposit history
+                  </p>
+                </div>
+              </Link>
+              <Link>
+                <div className="">
+                  <img
+                    className="w-12 h-12 m-auto"
+                    src="https://hgzy.org/assets/png/withdrawHistory-fb2bafcf.png"
+                    alt=""
+                  />
+                  <p className="text-sm w-12 text-center text-text">
+                    Withdrawal history
+                  </p>
+                </div>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="px-2 sm:px-4">
+          <div className=""></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default WalletBalance;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,31 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Login from "../Pages/Login";
-import Home from '../Pages/Home';
+import Home from "../Pages/Home";
 
 import Register from "../Pages/Register";
 import ForgotPassword from "../Pages/ForgotPassword";
-
+import Activity from "../Pages/Activity";
+import Wallet from "../Pages/Wallet";
 
 const router = createBrowserRouter([
-
-    {
-        path:'/',
-        element:<MainLayout/>,
-        children:[
-           { path:'/',element:<Home/>},
-        
-        
-        ]
-            
-    },
-    { path:'/login',element:<Login/>},
-    {path:"/register",element:<Register/>},
-    {path:"/forgot",element:<ForgotPassword/>},
-    
-])  
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [{ path: "/", element: <Home /> }],
+  },
+  { path: "/activity", element: <Activity /> },
+  { path: "/wallet", element: <Wallet /> },
+  { path: "/login", element: <Login /> },
+  { path: "/register", element: <Register /> },
+  { path: "/forgot", element: <ForgotPassword /> },
+]);
 
 export default router;
-
-
-
